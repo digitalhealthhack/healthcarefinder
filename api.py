3864cd6f98f0fc618b26a119f0f73dfbbdfdfd82
reedmace <jadhav.rahul@gmail.com>
2014-07-13 00:57:03 +0100

    modified json array structure for better navigation and compatibility with 'jlord' map scripts

--- api.py
+++ api.py
@@ -1,13 +1,11 @@
 import json
 import random
-from flask import Flask
+from flask import Flask, Response
 from flask.ext.restful import Api
 from flask.ext.restful import Resource
 
-
 APP = Flask(__name__)
 API = Api(APP)
-
 
 class HealthCenters(Resource):
     def get(self, latitude, longitude):
@@ -16,19 +14,17 @@ class HealthCenters(Resource):
             data = json.loads(json_file.read())
             for datum in data:
                 health_centers.append({
-                    datum['hospital']: {
-                        'latitude': datum['lat'],
-                        'longitude': datum['lng'],
-                        'address': datum['address'],
-                        'estimated': random.randint(90, 900)
-                    }
+                    'name': datum['hospital'],
+                    'lat': datum['lat'],
+                    'long': datum['lng'],
+                    'address': datum['address'],
+                    'estimated': random.randint(90, 900)
+                    
                 })
 
-        return health_centers
-
+        return Response(json.dumps(health_centers),  mimetype='application/javascript')
 
 API.add_resource(HealthCenters, '/<string:latitude>/<string:longitude>')
-
 
 if __name__ == '__main__':
     APP.run(debug=True)
